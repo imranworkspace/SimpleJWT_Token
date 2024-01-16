471d8c09a508d201164cdad712fd24aaf12c3507
imranworkspace <imranlatur24studymaterial@gmail.com>
2024-01-16 19:53:58 +0530

    token generated successfully

--- simplejwt_token/simplejwt_token/urls.py
+++ simplejwt_token/simplejwt_token/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('gettoken/',TokenObtainPairView.as_view(),name='gettoken'),
+    path('refresh_token/',TokenRefreshView.as_view()),
+    path('verfiy_token/',TokenRefreshView.as_view()),
 ]
